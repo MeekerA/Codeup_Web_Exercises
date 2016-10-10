0744b346e54d066a02c6d8c06124ced1573e18b2
Austin Meeker <austinmeeker@me.com>
2016-10-10 16:58:39 -0500

    JavaScript Dialog Functions for User Interaction exercise.

---
+++ js/user_interaction.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// TODO: Ask the user for their name.
+//       Keep asking if an empty input is provided.
+
+var name = prompt("Hi there! What is your name?");
+
+
+// TODO: Show an alert message that welcomes the user based on their input.
+
+// TODO: Ask the user if they like pizza.
+//       Based on their answer show a creative alert message.
