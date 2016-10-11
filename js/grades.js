e4cf208dd58d169f757054bdfb5a63ef0b385568
Austin Meeker <austinmeeker@me.com>
2016-10-11 10:59:54 -0500

    JavaScript Dialog Functions for User Interaction exercise.

---
+++ js/grades.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Austin on 10/11/16.
+ */
+
+"use strict";
+
+function averageGrade() {
+    var testOne
+    var testTwo
+    var testThree
+    return (testOne + testTwo + testThree) / 3;
+}
+
+for (var i = 1; i <= 3; i++) {
+    var result = prompt("What was the grade of Test #" + i + ".");
+}
+
+
+function genericAverageGrade() {
+
+}
+
--- js/user_interaction.js
+++ js/user_interaction.js
@@ -2,11 +2,21 @@
 
 // TODO: Ask the user for their name.
 //       Keep asking if an empty input is provided.
-
-var name = prompt("Hi there! What is your name?");
-
+do {
+    var userName = prompt("Hi there! What is your name?");
+} while (userName == null || userName.trim() == "");
 
 // TODO: Show an alert message that welcomes the user based on their input.
 
+alert("Nice to meet you " + userName + "!");
+
 // TODO: Ask the user if they like pizza.
 //       Based on their answer show a creative alert message.
+
+var pizza = confirm("Dude! Do you like Pizza?");
+
+if (pizza) {
+    alert("Awesome me too!");
+} else {
+    alert("That's no fun......");
+}
